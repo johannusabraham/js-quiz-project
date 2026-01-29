9df76d46aeba4e891b2730c7b8df67b2a77fe50f
johannusabraham <99084928@mydavinci.nl>
2026-01-29 11:47:31 +0100

    nieuwe zip folder en het vasthouden van html dingen die we later nog gaan gebruiken

---
+++ js-quiz-js.js
@@ -0,0 +1,33 @@
+const aantal_vragen = [
+    "Ben ik een mens?",
+    "Wat is de hoofdstad van Nederland?",
+    "Hoeveel dagen heeft een week?",
+    "hoelang duurt een dag?",
+    "hoeveel weken heeft een maand?"
+];
+
+const keuzes = [
+    ["Ja", "Nee"],
+    ["Amsterdam", "Rotterdam", "Den Haag"],
+    ["5", "7", "10"],
+    ["12 uur", "24 uur", "24 min"],
+    ["4 weken", "3 weken", "5 weken"]
+];
+
+const juiste_antwoorden = [
+    0, 
+    0, 
+    1,
+    1,
+    0
+];
+
+let huidige_vraag = 0;
+let score = 0;
+
+console.log("vragen:", aantal_vragen);
+console.log("keuzes:", keuzes);
+console.log("juiste antwoorden:", juiste_antwoorden);
+console.log("huidige vraag:", huidige_vraag);
+console.log("Score:", score);
+
